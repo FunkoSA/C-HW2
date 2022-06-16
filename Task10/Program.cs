8a40d091acec0333898d8a43bc9a15fb67fcd1bb
Funko Sergey <Funko@List.ru>
2022-06-16 05:39:35 +0300

    ADD HWC#2

---
+++ Task10/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+using System;
+using static System.Console;
+
+Clear();
+WriteLine("Программ принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
+Write("Введите трехзначное число:");
+string Number = ReadLine();
+while (Number.Length != 3)
+{
+    Write("Введите трехзначное число:");
+    Number = ReadLine();
+   
+}
+
+try
+{
+    int result = Int32.Parse(Number);
+    WriteLine ($"Вторая цифра числа {Number} является {Convert.ToInt32(Number)/10%10}");
+}
+catch (FormatException)
+{
+    WriteLine($"Введенное значение '{Number}' не является числом!");
+}
---
+++ Task13/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 6
+
+78 -> третьей цифры нет
+
+32679 -> 6
+
+*/
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите число:");
+string Number = ReadLine();
+int Len = Number.Length;
+
+try
+{
+    int result = Int32.Parse(Number);
+    
+    WriteLine (Len > 2 ? $"Третья цифра числа {Number} является {Convert.ToInt32(Number)/100%10}" : $"У введенного числа нет третьей цифры, оно меньше 100");
+    WriteLine (Convert.ToInt32(Number)/100 != 0 ? $"Третья цифра числа {Number} является {Convert.ToInt32(Number)/100%10}" : $"У введенного числа нет третьей цифры, оно меньше 100");
+}
+catch (FormatException)
+{
+    WriteLine($"Введенное значение '{Number}' не является числом!");
+}
